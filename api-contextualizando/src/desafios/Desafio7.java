97ae027400f3ef494a863a542b52942b7ec691da
vitor277 <vitor-gasques@hotmail.com>
2024-05-27 16:33:06 -0300

    Desafio 7 feito

---
+++ api-contextualizando/src/desafios/Desafio7.java
@@ -0,0 +1,14 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Desafio7 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        int segundoMaior = numeros.stream().sorted(Comparator.reverseOrder()).skip(1).findFirst().orElseThrow();
+        System.out.println(segundoMaior);
+    }
+}

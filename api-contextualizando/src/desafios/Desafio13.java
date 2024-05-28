23f8a7878d1acaf492f776b663f1f1489e599fd4
vitor277 <vitor-gasques@hotmail.com>
2024-05-27 22:21:00 -0300

    Desafios até 14 feitos

--- api-contextualizando/src/desafios/Desafio13.java
+++ api-contextualizando/src/desafios/Desafio13.java
@@ -2,13 +2,12 @@ package desafios;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Desafio11 {
+public class Desafio13 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        int somaQuadrado = numeros.stream().map(n-> n * n).reduce(0, Integer :: sum);
-        System.out.println(somaQuadrado);
+        numeros.stream().filter(n -> n < 10 && n > 5).forEach(System.out :: print);
+
     }
 }
---
+++ api-contextualizando/src/desafios/Desafio14.java
@@ -0,0 +1,24 @@
+package desafios;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Desafio14 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        List<Integer> primos = numeros.stream()//sorted(Comparator.reverseOrder());
+        .filter(n -> {
+            if (n <= 1) return false;
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) return false;
+            }
+            return true;
+        })
+        .collect(Collectors.toList());
+
+        System.out.println(primos.stream().max(Integer:: compareTo));
+    }
+}

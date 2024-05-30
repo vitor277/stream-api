397c9c16dc73f8d8d9660ded4addf014869186d1
vitor277 <vitor-gasques@hotmail.com>
2024-05-30 15:37:57 -0300

    Desafios 18 e 19 criados

--- api-contextualizando/src/desafios/Desafio18.java
+++ api-contextualizando/src/desafios/Desafio18.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class Desafio18 {
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1, 1);
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
         boolean numerosIguais = numeros.stream().noneMatch(n -> n.equals(n));
         System.out.println(numerosIguais);
     }
---
+++ api-contextualizando/src/desafios/Desafio19.java
@@ -0,0 +1,15 @@
+package desafios;
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio19 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3); 
+
+        int somaIntervalo = numeros.stream().filter(n -> n > 3 && n < 5).reduce(0, Integer :: sum);
+        System.out.println(somaIntervalo);
+    }
+    
+
+
+}
